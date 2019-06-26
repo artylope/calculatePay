f367f34bcc508f181089c051f9fdb34aba7c06be
Yi Xin Ang <angyixin@gmail.com>
2019-06-26 16:50:17 +0800

    Revert "first commit"
    This reverts commit 2c54005b3cb62e681324142796b76fb8c0b4b335.

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-console.log("hello script js");
+var gamesPlayed = 0;
+var gamesWon = 0;
+var gamesLost = 0;
+//generate a randomNumber between 1 to 100;
+var randomSecretNumber = Math.floor(Math.random()*100) + 1;
+console.log("secret number is " + randomSecretNumber);
 
-var maxHours = 40;
-var normalRate = 20;
-var hoursWorked;
-var totalPay;
-var normalPay;
-var overtimeHours;
-var overtimePay;
-var overtimeRate = 30;
 
-var calculatePay = function(hoursWorked){
-  if (hoursWorked > maxHours){
-    overtimeHours = hoursWorked - maxHours;
-    overtimePay = overtimeHours * overtimeRate;
-    normalPay = maxHours * normalRate;
-    totalPay = normalPay + overtimePay;
-    // return totalPay;
-    display("bob is paid $" + totalPay);
+// function signature
+// guessedNumber(number), secretNumber(number) --> won(boolean)
+//
+// function header:
+// hasWon( guessedNumber, secretNumber )
+
+var hasWon = function(guessedNumber, secretNumber){
+  if (guessedNumber === secretNumber){
+    display("You won");
+    gamesWon ++;
+    document.getElementById('games-won').textContent = gamesWon;
+  } else if (guessedNumber < secretNumber){
+    gamesLost ++;
+    document.getElementById('games-lost').textContent = gamesLost;
+    display("Try again. Number is higher");
   } else {
-    normalPay = hoursWorked * normalRate;
-    totalPay = normalPay;
-    // return totalPay;
-    display("bob is paid $" + totalPay);
+    display("Try again. Number is lower");
+    gamesLost ++;
+    document.getElementById('games-lost').textContent = gamesLost;
   }
 };
+
+var inputHappened = function(currentInput){
+  //check if number
+  // console.log(currentInput);
+  // console.log(typeof (currentInput));
+  checkIfNumber(currentInput);
+  if (checkIfNumber(currentInput) === true){
+    gamesPlayed ++;
+    document.getElementById('games-played').textContent = gamesPlayed;
+    currentInput = parseInt(currentInput);
+    hasWon(currentInput,randomSecretNumber);
+    if(currentInput>100 || currentInput === 0){
+      display("Please type a number between 1 to 100");
+    }
+  } else {
+    display("Please type a number");
+  }
+};
+
+var checkIfNumber = function(input) {
+    if (!isNaN(input) === true) {
+      // console.log(input + " is a number");
+      return true;
+    } else {
+      // console.log(input + " is not a number");
+      return false;
+    }
+  };
